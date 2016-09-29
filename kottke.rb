cacaca85ec83446dd9e8b47d49e8c6395043ac04
pca2 <atwaterc@gmail.com>
2016-09-29 00:06:55 -0400

    Add better check for youtube videos add better link parsing add support for multiple links per post

--- kottke.rb
+++ kottke.rb
@@ -5,17 +5,26 @@ require 'open-uri'
 url = 'http://feeds.kottke.org/main'
 
 feed = RSS::Parser.parse(open(url))
+#file = (File.readlines('main')).join(" ")
+#feed = RSS::Parser.parse(file)
 
-
-feed.entries.each do |post| 
-  binding.pry if defined? Pry
-  if !post.content.content.include?("tag/video")
+links = []
+feed.entries.each_with_index do |post, i| 
+  #Probably want to check for non-https
+  if post.content.content[/="https:\/\/www.youtube.com.*?\"/].nil?
    puts "not a video post"
    next
   end
- yt = post.content.content[/youtube.*rel=0/]
- yt_id = yt[/embed\/.*\?/].gsub(/embed\/|\?/, "")
- puts "the link for this video is https://www.youtube.com/watch?v=#{yt_id}"
+  links = post.content.content.scan(/youtube.*?\"/)
+  links.map {|l| l.gsub!(/\?rel.*|\"/,"")}
+  #Next: Parse the ID?
+  #Store in a DB?
+  binding.pry if defined? Pry
+ #links << yt
+ #puts links
+ puts  "#{post.title.content} link: " + links.join(" ")
+ #yt_id = yt[/embed\/.*\?/].gsub(/embed\/|\?/, "")
+ #puts "the link for this video is https://www.youtube.com/watch?v=#{yt_id}"
 
 end
 
