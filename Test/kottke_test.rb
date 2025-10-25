99c172a7d6087f57b6a65e4396b106602e9310b9
Carleton Atwater <atwaterc@gmail.com>
2025-10-25 08:29:28 -0400

    update test to use assert_equal instead of custom checker.
    should update other tests too

--- Test/kottke_test.rb
+++ Test/kottke_test.rb
@@ -181,14 +181,13 @@ class KottkeTest < Minitest::Test
       authorize_yt(CLIENT_ID,CLIENT_SECRET)
       account = define_account(REFRESH_TOKEN)
       dummy_playlist = create_dummy_playlist(account)
-      new_item = append_to_playlist(dummy_playlist, SAMPLE_VID_ID_ONE)
-      new_item = append_to_playlist(dummy_playlist, SAMPLE_VID_ID_TWO)
+      append_to_playlist(dummy_playlist, SAMPLE_VID_ID_ONE)
+      append_to_playlist(dummy_playlist, SAMPLE_VID_ID_TWO)
       feed = get_sample_feed('twoembed.xml')
       process_feed(feed, nil,dummy_playlist)
       reorder_any_new_vids(@new_items)
       playlist_array = get_playlist_vids(dummy_playlist)
-      check_result = check_vid_arrays_match(playlist_array,NEW_VID_ON_TOP_LIST)
-      assert check_result, "playlist and NEW_VID_ON_TOP_LIST arrays should be identical"
+      assert_equal(NEW_VID_ON_TOP_LIST.sort, playlist_array.sort, msg: "playlist and NEW_VID_ON_TOP_LIST arrays should be identical")
       delete_playlist(dummy_playlist)
     end
   end
